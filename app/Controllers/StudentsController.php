8b8c856b597dab98f5314d5c869a5df2d0cd4697
unknown <ggwepq@gmail.com>
2024-08-18 14:52:32 +0800

    git init

---
+++ app/Controllers/StudentsController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use CodeIgniter\HTTP\ResponseInterface;
+
+class StudentsController extends BaseController
+{
+    public function listStudentRecord()
+    {
+        // List all student record 
+        return view('Student/list');
+    }
+
+    public function createStudentRecord()
+    {
+        // Create or Add new student record
+        return view('Student/add');
+    }
+
+    public function editStudentRecord($id)
+    {
+        // Show details to be edited in the view.php
+        return view('Student/edit');
+    }
+
+    public function updateStudentRecord($id)
+    {
+        // Update the student record on the Database
+    }
+
+    public function deleteStudentRecord($id)
+    {
+        // delete a student record on the Database
+    }
+}
